21adcfde181003159dea1b40fc436811ed2e0836
Andy8421 <872029056@qq.com>
2019-07-17 18:57:41 +0800

    first

--- src/mypro01/Happy.java
+++ src/mypro01/Happy.java
@@ -4,6 +4,6 @@ public class Happy {
 	public static void main(String[] args) {
 		System.out.println("test commit");
 		System.out.println("test commit");
-		System.out.println("外部人员修改");
+		System.out.println("主人修改");
 	}
 }
